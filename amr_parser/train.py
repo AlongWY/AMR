906cf56f46937e8b28bb419e0f638aa28087f6d9
ylfeng <ylfeng@ir.hit.edu.cn>
2020-06-01 17:44:22 +0800

    fix name

--- amr_parser/train.py
+++ amr_parser/train.py
@@ -15,10 +15,10 @@ from amr_parser.work import parse_data
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-console_handler = logging.FileHandler('train.log')
+file_handler = logging.FileHandler('train.log')
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def parse_config():
