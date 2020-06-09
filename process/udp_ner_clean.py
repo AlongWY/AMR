e975b08beb30efb06831d83dfad4a31fd6b066a4
ylfeng <ylfeng@ir.hit.edu.cn>
2020-06-09 13:57:54 +0800

    clean ner

---
+++ process/udp_ner_clean.py
@@ -0,0 +1,56 @@
+import json, re
+from argparse import ArgumentParser
+from collections import defaultdict
+
+URL = pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F])|[\-])+')
+
+
+def clean(word: str):
+    word = word.replace('“', "\"") \
+        .replace('”', "\"") \
+        .replace('’', "'") \
+        .replace('‘', "'") \
+        .replace('1/2', "½") \
+        .replace('1/3', "⅓")
+    # word = re.sub(URL, '[URL]', word)
+    word = re.sub(r'[–—-]+', '-', word)
+    word = re.sub(r'[.…]+', '…', word)
+    return word
+
+
+def main(args):
+    res = {}
+    with open(args.input, encoding='utf-8') as f, open(args.output, mode='w', encoding='utf-8') as out:
+        for line in f.readlines():
+            mrp_json = json.loads(line)
+            mrp_token = [clean(tok) for tok in mrp_json['tok']]
+            src_token = [node['label'] for node in mrp_json['nodes']]
+            src_tok = [clean(node) for node in src_token]
+
+            if mrp_token == src_tok:
+                continue
+
+            src_tok_set = set(enumerate(src_tok))
+            core_tok_Set = set(enumerate(mrp_token))
+
+            difference = list(src_tok_set.difference(core_tok_Set))
+
+            print(difference)
+
+            to_clean = {
+                'src_tok': src_token,
+                'ner_tok': mrp_json['tok'],
+                'ner': mrp_json['ner'],
+            }
+
+            res[mrp_json['id']] = to_clean
+        out.write(json.dumps(res, ensure_ascii=False) + '\n')
+
+
+if __name__ == '__main__':
+    argparser = ArgumentParser()
+    argparser.add_argument('--input', '-i', required=True)
+    argparser.add_argument('--output', '-o', required=True)
+    args = argparser.parse_args()
+
+    main(args)
