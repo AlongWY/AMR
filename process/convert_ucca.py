734a7a7986a308f0836ea9bbfab772574d067667
ylfeng <ylfeng@ir.hit.edu.cn>
2020-07-08 12:43:23 +0800

    fix ucca

--- process/convert_ucca.py
+++ process/convert_ucca.py
@@ -27,6 +27,21 @@ def generate_amr_lines(f1, f2):
         break
 
 
+def get_anchors(word, snt):
+    # snt = snt.lower()
+    word = word.lower()
+
+    if len(word) >= 3 and word.startswith('\"') and word.endswith('\"'):
+        word = word.strip('\"')
+
+    pattern = re.escape(word)
+    for match in re.finditer(f"{pattern}(\\s|$|\\W)", snt):
+        return match.start(), match.start() + len(word)
+
+    start = snt.index(word)
+    return start, start + len(word)
+
+
 def main(args):
     with open(args.input, encoding='utf-8') as f, \
             open(args.extra, encoding='utf-8') as e, \
@@ -55,36 +70,21 @@ def main(args):
                 tgt: str
                 id_ = int(src[1:])
                 node_map[id_] = index
-                tgt = tgt.lower()
-                if len(tgt) >= 3 and tgt.startswith('\"') and tgt.endswith('\"'):
-                    tgt = tgt.strip('\"')
                 try:
-                    pattern = re.escape(tgt)
-                    new_nodes = []
-                    for match in re.finditer(f"{pattern}(\\s|$|\\W)", snt):
-                        new_nodes.append({
-                            'id': id_,
-                            # 'tgt': tgt,
-                            "anchors": [{
-                                "from": match.start(),
-                                "end": match.start() + len(tgt)
-                            }]
-                        })
-                        break
-                    if len(new_nodes):
-                        nodes.extend(new_nodes)
-                    else:
-                        start = snt.index(tgt)
-                        nodes.append({
-                            'id': id_,
-                            # 'tgt': tgt,
-                            "anchors": [{
-                                "from": start,
-                                "end": start + len(tgt)
-                            }]
-                        })
+                    start, end = get_anchors(tgt, snt)
+                    nodes.append({
+                        'id': id_,
+                        # 'tgt': tgt,
+                        "anchors": [{
+                            "from": start,
+                            "end": end
+                        }]
+                    })
                 except Exception as e:
-                    nodes.append({'id': id_})
+                    nodes.append({
+                        'id': id_,
+                        'tgt': tgt
+                    })
                     if tgt != '[unreal]' and tgt != '[multi]':
                         print(tgt)
 
@@ -95,17 +95,23 @@ def main(args):
                 label: str = role[1:]
                 source = int(src[1:])
                 target = int(tgt[1:])
-                if label.startswith("op"):
-                    nodes[node_map[source]].setdefault("anchors", [])
-                    nodes[node_map[source]]['anchors'].extend(nodes[node_map[target]].get('anchors', []))
-                    removed.append(target)
-                    removed_map[target] = source
-                else:
-                    edges.append({
-                        "source": source,
-                        "target": target,
-                        "label": label
-                    })
+                edges.append({
+                    "source": source,
+                    "target": target,
+                    "label": label
+                })
+
+            for src, role, tgt in ucca1.attributes():
+                src = int(src[1:])
+                nodes[node_map[src]].setdefault('anchors', [])
+                start, end = get_anchors(tgt, snt)
+                nodes[node_map[src]]['anchors'].append(
+                    {
+                        "from": start,
+                        "end": end
+                    }
+                )
+
             nodes = [node for node in nodes if node['id'] not in removed]
             remap = {node['id']: index for index, node in enumerate(nodes)}
             if remap != node_map:
@@ -117,8 +123,6 @@ def main(args):
 
                     edge["source"] = remap[source]
                     edge["target"] = remap[target]
-            for node in nodes:
-                node['anchors'] = sorted(node['anchors'], key='source')
 
             out.write(json.dumps({
                 "id": metadata['id'],
