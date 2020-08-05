819af12f7f791a9788ca0b70bd5777f66c3e9eba
ylfeng <ylfeng@ir.hit.edu.cn>
2020-08-05 12:26:46 +0800

    update eval

--- eval.sh
+++ eval.sh
@@ -4,8 +4,8 @@
 
 if [ $# -eq 1 ]; then
   CUDA_VISIBLE_DEVICES=$1 PYTHONPATH=. python amr_parser/work.py \
-    --load_path amr_ckpt/epoch535_batch154999 \
-    --test_data data/amr/amr.test.convert.input_clean.recategorize.nosense
+    --load_path amr_ckpt/epoch401_batch115999 \
+    --test_data data/test.input.recategorize
 else
   echo "$0 [CUDA DEVICES]"
 fi
