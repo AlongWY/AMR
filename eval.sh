5b003fc0bf2d2503eb95cbfda302871a2c00e43b
ylfeng <ylfeng@ir.hit.edu.cn>
2020-08-05 14:40:08 +0800

    update eval

---
+++ eval.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# -*- coding: utf-8 -*_
+# Author: Yunlong Feng <ylfeng@ir.hit.edu.cn>
+
+if [ $# -eq 1 ]; then
+  CUDA_VISIBLE_DEVICES=$1 PYTHONPATH=. python amr_parser/work.py \
+    --load_path epoch749_batch20999_dev_out \
+    --test_data data/test.input
+else
+  echo "$0 [CUDA DEVICES]"
+fi
