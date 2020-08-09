e26c1c09d3428ddebab05a8ceaf566708995f39e
ylfeng <ylfeng@ir.hit.edu.cn>
2020-08-09 16:11:44 +0800

    use best drg

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 
 if [ $# -eq 1 ]; then
   CUDA_VISIBLE_DEVICES=$1 PYTHONPATH=. python amr_parser/work.py \
-    --load_path drg_deu_ckpt/epoch20499_batch81999 \
+    --load_path drg_deu_all_ckpt/epoch7708_batch34999 \
     --test_data data/drg_deu.input
 else
   echo "$0 [CUDA DEVICES]"
